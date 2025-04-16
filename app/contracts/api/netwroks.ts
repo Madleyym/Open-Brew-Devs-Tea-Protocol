61032e782eb837165a41d7209a9e676bb139767a
Madleyym <tryandaasu@gmail.com>
2025-04-16 23:37:40 +0700

    Add teaSepoliaNetwork configuration to networks.ts

---
+++ app/contracts/api/netwroks.ts
@@ -0,0 +1,19 @@
+export const teaSepoliaNetwork = {
+  networkName: "Tea Sepolia",
+  chainId: 10218,
+  currencySymbol: "TEA",
+  rpcUrl: "https://tea-sepolia.g.alchemy.com/public",
+  explorerUrl: "https://sepolia.tea.xyz",
+  contractAddresses: {
+    staking_OLD: "0xbba4121A3bDE406Be131257C8918F67c97789166", 
+    staking: "0x64B13eD4Af86Bc8CdF741A1a1291b5baE0DD14aF", 
+    dex: "0xC6200C2eb0efB83602a40aCaa842Cc1E3e06190C", 
+    BrewToken: "0x6C535Fd8aab652dD5284e3568208EF9FdCd21232", 
+    BrewETH: "0x04dF97d2565C3821B6b3a56fCD405084f0239324", 
+    opBREW: "0x97EC8BcB2c8dA0b119A5263798bB5dB332C917Fb", 
+    BrewNFT: "0x1dC5474E984A29e2B7835B6045465bD5a4D58DfE", 
+    BrewQuest: "0xe2124e251346e10a2f1095561A96AF52C6f182d0", 
+    OpenBrewSender: "0x69eb499c045b9931ad8732a46f820400c1bee235", 
+    OpenBrewForumTEA: "0x085bd9c4b9a6026125b0373382c37cda6d0bd19a", 
+  },
+};
